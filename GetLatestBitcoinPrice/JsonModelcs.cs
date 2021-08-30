2d1bf95f528fd741faa8536b120d0de08ff3ea2d
ArifMatin <arif.matin@mail.itsligo.ie>
2021-08-30 22:38:07 +0100

    finished

---
+++ GetLatestBitcoinPrice/JsonModelcs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetLatestBitcoinPrice
+{
+    public class JsonModelcs
+    {
+
+    }
+    public class Time
+    {
+        public int MyProperty { get; set; }
+    }
+}
--- GetLatestBitcoinPrice/Program.cs
+++ GetLatestBitcoinPrice/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +13,47 @@ namespace GetLatestBitcoinPrice
         static void Main(string[] args)
         {
 
+            var data = GetJsonDataAsync().GetAwaiter().GetResult();
 
+            if (data == null)
+            {
+                Console.WriteLine("Error");
+            }
+
+            Console.WriteLine($"BTC Price EUR {data}.");
+
+            Console.ReadKey();
+        }
+
+        private static async Task<string> GetJsonDataAsync()
+        {
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync("https://api.coindesk.com/v1/bpi/currentprice.json");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                
+                return ParseJson(jsonData);
+            }
+
+            return null;
+        }
+
+        private static string ParseJson(string data)
+        {
+            JObject jsonObject = JObject.Parse(data);
+
+            var chartName = (string)jsonObject["chartName"];
+            var code = (string)jsonObject["bpi"]["EUR"]["code"];
+            var rate = (string)jsonObject["bpi"]["EUR"]["rate"];
+
+            if (chartName == "Bitcoin" && code == "EUR")
+            {
+                return rate;
+            }
+
+            return null; 
         }
     }
 }
